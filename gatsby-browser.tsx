5c648d2ae299fa481c6e5785a6f859fce7eed7aa
alvarodelpazo <alvarodelpazo@gmail.com>
2023-12-20 10:42:13 +0000

    refactor: elimina contenido innecesario

--- gatsby-browser.tsx
+++ gatsby-browser.tsx
@@ -1,7 +1,3 @@
-import React from 'react';
-
-import { GatsbyBrowser } from 'gatsby';
-
 import { initI18next } from './src/utils/i18next';
 
 /**
@@ -17,25 +13,3 @@ export const onClientEntry = (): void => {
   initI18next();
   document.dispatchEvent(new Event('gatsbyOnClientEntry'));
 };
-
-// Called when the user changes routes
-// @see https://www.gatsbyjs.org/docs/browser-apis/#onRouteUpdate
-export const onRouteUpdate = (): void => {
-  document.dispatchEvent(new Event('gatsbyOnRouteUpdate'));
-};
-
-// Between these two events, React render/rehydrate happens.
-
-// Called when the initial (but not subsequent) render of Gatsby App is *done* on the client.
-// This is called after render happens, and only once, on first render
-// @see https://www.gatsbyjs.org/docs/browser-apis/#onInitialClientRender
-export const onInitialClientRender = (): void => {
-  document.dispatchEvent(new Event('utag-loaded'));
-};
-
-export const wrapRootElement: GatsbyBrowser['wrapRootElement'] = ({ element }) => <>{element}</>;
-
-export const wrapPageElement: GatsbyBrowser<Record<string, unknown>>['wrapPageElement'] = ({
-  element,
-  props,
-}) => <>{element}</>;
